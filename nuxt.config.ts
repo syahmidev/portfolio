78a02077f5c64a4da085f945c6c902e846bf23fa
syahmidev <wanahmadsyahmi26@gmail.com>
2024-11-15 09:19:59 +0800

    SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+        }
+    },
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
     css: ["@/assets/main.css", "animate.css/animate.min.css"],
--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,21 @@
         <SocialMediaSection />
     </div>
 </template>
+
+<!-- SEO things -->
+<script setup>
+    useHead({
+        title: 'Syahmi - Full Stack Developer',
+        meta: [
+            { name: 'description', content: 'Ambitious and dedicated Senior Full Stack Web Developer with progressive experience from junior to senior-level roles. Passionate about expanding knowledge in advanced web development, server management, and critical thinking. Eager to achieve expertise in backend server management and lead teams to deliver robust, scalable web solutions.' }
+        ],
+    })
+
+    useSeoMeta({
+        title: 'Syahmi - Full Stack Developer',
+        ogTitle: 'Syahmi - Full Stack Developer',
+        description: 'Ambitious and dedicated Senior Full Stack Web Developer with progressive experience from junior to senior-level roles. Passionate about expanding knowledge in advanced web development, server management, and critical thinking. Eager to achieve expertise in backend server management and lead teams to deliver robust, scalable web solutions.',
+        ogDescription: 'Ambitious and dedicated Senior Full Stack Web Developer with progressive experience from junior to senior-level roles. Passionate about expanding knowledge in advanced web development, server management, and critical thinking. Eager to achieve expertise in backend server management and lead teams to deliver robust, scalable web solutions.',
+        ogImage: '/favicon.png',
+    })
+</script>
